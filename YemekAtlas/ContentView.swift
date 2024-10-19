32af5f6518c7975865aa948c48ff6ea0669104f0
Selahattin Edin <selahattinedin1@gmail.com>
2024-10-19 15:00:45 +0300

    Initial Commit

---
+++ YemekAtlas/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  YemekAtlas
+//
+//  Created by Selahattin EDİN on 19.10.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ YemekAtlas/YemekAtlasApp.swift
@@ -0,0 +1,17 @@
+//
+//  YemekAtlasApp.swift
+//  YemekAtlas
+//
+//  Created by Selahattin EDİN on 19.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct YemekAtlasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
