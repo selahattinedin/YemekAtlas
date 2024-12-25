a25955820437b5810c8ffe29de139e449ade8e4b
Selahattin Edin <selahattinedin1@gmail.com>
2024-12-25 19:16:11 +0300

    The "Logout" button has been added, and the "Popular Meal" feature is provided as a default, which will be updated in the future.

---
+++ YemekAtlas/Views/PopularRecipesView.swift
@@ -0,0 +1,18 @@
+//
+//  PopularRecipesView.swift
+//  YemekAtlas
+//
+//  Created by Selahattin EDİN on 25.12.2024.
+//
+
+import SwiftUI
+
+struct PopularRecipesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PopularRecipesView()
+}
