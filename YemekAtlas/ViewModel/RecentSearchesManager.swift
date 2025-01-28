3cc0fae749353da69cfcb7d8d5b4fe916ce0c6b9
Selahattin Edin <selahattinedin1@gmail.com>
2025-01-28 23:47:33 +0300

    The food search history has been added to the SearchView.

---
+++ YemekAtlas/ViewModel/RecentSearchesManager.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  YemekAtlas
+//
+//  Created by Selahattin EDİN on 28.01.2025.
+//
+
+import Foundation
---
+++ YemekAtlas/Views/RecentSearchesView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  YemekAtlas
+//
+//  Created by Selahattin EDİN on 28.01.2025.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
